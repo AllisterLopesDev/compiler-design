1f92e34b160ee56bb4200c72403455753fd84988
AllisterLopesDev <dev.allisterlopes@gmail.com>
2022-11-04 11:42:42 +0530

    implementation of counter

--- src/Experiment_1/LexicalAnalysis.java
+++ src/Experiment_1/LexicalAnalysis.java
@@ -1,14 +1,16 @@
 package Experiment_1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class LexicalAnalysis {
 
     public static void main(String[] args) {
 
-//        char[] identifier = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-//                'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'z'};
-
         String[] id = new String[25];
-        int a=0;// Sorted Array of 100
+        int a=0;// index of the array
         for (char i = 'a'; i < 'z'; i++) {
             id[a] = String.valueOf(i);
             a++;
@@ -16,9 +18,8 @@ public class LexicalAnalysis {
 
         String[] operator = {"+","-","*","/","<",">"};
 
-//        String[] literal = {"1","2","3","4","5","6","7","8","9","10"};
         int n = 10000;
-        String[] literal = new String[n];                       // Sorted Array of 100
+        String[] literal = new String[n];                       // Declaraion of litral array
         for (int i = 0; i < literal.length; i++) {
             literal[i] = String.valueOf(i + 1);
         }
@@ -27,22 +28,32 @@ public class LexicalAnalysis {
                 "or", "try", "while", "do", "catch"};
 
         String[] symbol = {"!","=","@",";","%","(",")","{","}"};
+        Scanner in = new Scanner(System.in);
 
-        String input = "if ( a > b ) + a - 12 + 1";
+        System.out.println("Enter the expression with space after each character");
+        System.out.println("Eg. if ( a > 10 ) + a");
+        String input = in.nextLine();
+//        String input = "if ( a > b ) + a - b + 1";
 
         String[] toStringArray = null;
         toStringArray = input.split(" ");
 
-        int idCounter = 1;
+        int idCounter = 0;
 
         String[] storeId = new String[idCounter];
         int symbolCounter = 1;
-
+        ArrayList<String> temp = new ArrayList<>(); // Declaration of dynamic array
         for (int i = 0; i < toStringArray.length; i++) {
             for (String s : id) {
-                if (toStringArray[i].equals(s)) {
-                    System.out.println(toStringArray[i] + " -> id " + idCounter);
-                    idCounter++;
+
+                String val = toStringArray[i];
+                if (val.equals(s)) {
+                    if (!temp.equals(val)){
+                        temp.add(val);
+                        int idCount = temp.indexOf(val)+1;
+                        System.out.println(temp.get(temp.indexOf(val)) +" --> id "+idCount);
+                    }
+
                 }
             }
             for (String s : operator) {
